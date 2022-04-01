7b6f62709b4031765f87a0002c244b9cd9289ed3
irina-cosovan <74117551+irina-cosovan@users.noreply.github.com>
2022-04-01 15:29:27 +0300

    Add files via upload

---
+++ proiectPIU/Clase/Gen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proiectPIU.Clase
+{
+    class Gen
+    {
+        public Gen(int _idGen, string _nume)
+        {
+            ID_Gen = _idGen;
+            Nume = _nume;
+        }
+        public int ID_Gen { get; set; }
+        public string Nume { get; set; }
+    }
+}
---
+++ proiectPIU/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proiectPIU
+{
+    //aici apelam clasele si construim main-ul
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
